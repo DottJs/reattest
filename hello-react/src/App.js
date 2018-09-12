72a1da0c64185584770b33595d5cda260f15d655
devdott <adzxcvb@naver.com>
2018-09-12 17:32:14 +0900

    prop, propTypes, state, setState
    3장

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // 생성한 컴포넌트에서 prop으로 잡은 것 = {표시할 내용}}
+      <MyComponent name='Kim' age={20}/> //{'Kim'} JS코드안의 문자열이란 의미로 
     );
   }
 }
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+
+    //자신의 기본값을 설정하였다. // defaultProps
+    //직접 값을 주는 것은 부모 Component 이다.
+    static defaultProps = { 
+        name : '기본이름',
+        age: 0
+    }
+
+    static propTypes = {
+        name : PropTypes.string,
+        age : PropTypes.number.isRequired 
+        //number / array / bool / func / object / string / symbol / node / element / instanceOf(MYClass) / oneOf(['male', 'Female']) / oneOfType() / arrayOf() / objectOf() / shape() / any
+        //isRequired 필수요소 값으로 설정
+    }
+    
+    //변화하는 값을 사용하기 위해서는 state 를 사용한다.
+    //값을 업데이트 하기 위해서는 setState를 사용해야한다.
+    constructor(props){
+        super(props);
+        this.state = {
+            number : 0,
+        }
+    }
+    
+    render(){
+        return (
+            <div>
+                <p>저는 {this.props.name} 입니다.</p>
+                <p>저는 {this.props.age} 살입니다.</p>
+                <p>숫자 : {this.state.number}</p>
+                <button onClick={()=>{
+                    this.setState({
+                        number : this.state.number + 1
+                    })
+                }
+                }> 더하기 </button>
+            </div>
+        )
+    }
+}
+
+export default MyComponent; 
