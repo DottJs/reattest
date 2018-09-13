53b5c6b129c5d10c07c2c557dfe40856303e8c64
devdott <adzxcvb@naver.com>
2018-09-13 11:20:07 +0900

    다수의 이벤트 핸들링 사용
    setState({키:값}) 을 변경하는데에 핸들링에서 [키]를 입력한다

--- hello-react/src/eventPart.js
+++ hello-react/src/eventPart.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 
 class EventPart extends Component{
     state = {
+        username : '',
         message : ''
     }
 
     handleChang = (e) => {
         this.setState({
-            message : e.target.value
+            [e.target.name] : e.target.value
         });
     }
 
@@ -19,7 +20,9 @@ class EventPart extends Component{
     render(){
         return(
             <div>
-                <input type="text" name="message" value={this.state.message} placeholder="값" onChange={this.handleChang}>
+                <input type="text" name="message" value={this.state.message} placeholder="값message" onChange={this.handleChang}>
+                </input>
+                <input type="text" name="username" value={this.state.message} placeholder="값username" onChange={this.handleChang}>
                 </input>
                 <button onClick={this.handleOnClick}>
                 버튼</button>
